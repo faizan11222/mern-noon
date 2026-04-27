2dd601b74a0569195af7e2373fe2db5be99ed365
faizan <62203593+faizan11222@users.noreply.github.com>
2026-04-27 15:59:17 +0500

    v3

--- studentfrontend/src/pages/Homepage.jsx
+++ studentfrontend/src/pages/Homepage.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 const HomePage = () => {
+    //hook for getting students data
+    const [students, setStudents] = useState([]);
+    //hook for loading state
+    const [loading, setLoading] = useState(false);
+    //hook for error handling
+    const [error, setError] = useState(null);
     return (
         <div>
             <h1>Homepage</h1>
