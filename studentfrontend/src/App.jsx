2b4cda65faf4bcbe7d46e28d18ae4bfc063e71d9
faizan <62203593+faizan11222@users.noreply.github.com>
2026-04-27 14:39:13 +0500

    frontend start

---
+++ studentfrontend/src/App.jsx
@@ -0,0 +1,12 @@
+import BasicExample from "./components/navbar"
+
+
+function App() {
+ 
+
+  return (
+   <BasicExample />
+  )
+}
+
+export default App
---
+++ studentfrontend/src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function BasicExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Student Management System</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
