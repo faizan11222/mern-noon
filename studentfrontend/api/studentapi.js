7ac1f45f45786bcd8115d0ea85d4e42e832629ed
faizan <62203593+faizan11222@users.noreply.github.com>
2026-04-28 13:34:41 +0500

    update1

--- studentfrontend/api/studentapi.js
+++ studentfrontend/api/studentapi.js
@@ -8,4 +8,16 @@ const api = axios.create({
 })
 
 //creating a function that is getting all students API
-export const getAllStudents = async() => (await api.get('/students/showstudents')).data
+export const getAllStudents = async() => (await api.get('/api/students/showstudents')).data
+
+//creating a function that is getting all students API
+export const getStudentById = async(id) => (await api.get(`/api/students/student/${id}`)).data
+
+//creating a function to add students API
+export const addStudent = async(data) => (await api.post('/api/students/addstudent', data)).data
+
+//creating a function to update students API
+export const updateStudent = async(id,data) => (await api.post(`/api/students/updatestudent/${id}`, data)).data
+
+//creating a function to delete students API
+export const deleteStudent = async(id) => (await api.delete(`/api/students/deletestudent/${id}`)).data
